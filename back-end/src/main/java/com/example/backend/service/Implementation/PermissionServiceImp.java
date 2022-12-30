3c743df287ca66a3441e784cee05088c393930bc
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-30 19:44:43 +0100

    create service interfaces

---
+++ back-end/src/main/java/com/example/backend/service/Implementation/PermissionServiceImp.java
@@ -0,0 +1,7 @@
+package com.example.backend.service.Implementation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PermissionServiceImp {
+}
---
+++ back-end/src/main/java/com/example/backend/service/Interface/UserService.java
@@ -0,0 +1,4 @@
+package com.example.backend.service.Interface;
+
+public interface UserService {
+}
