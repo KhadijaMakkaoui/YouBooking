7556c91d1be7c798f0925281125e477e1fa43a96
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-01-02 14:29:55 +0100

    implemt methods in Chambre Service

--- back-end/src/main/java/com/example/backend/service/Implementation/ChambreServiceImp.java
+++ back-end/src/main/java/com/example/backend/service/Implementation/ChambreServiceImp.java
@@ -1,7 +1,43 @@
 package com.example.backend.service.Implementation;
 
+import com.example.backend.entity.Chambre;
+import com.example.backend.service.Interface.ChambreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ChambreServiceImp {
+public class ChambreServiceImp implements ChambreService {
+    ChambreService chambreService;
+
+    public ChambreServiceImp(ChambreService chambreService) {
+        this.chambreService = chambreService;
+    }
+
+    @Override
+    public Chambre addChambre(Chambre chambre) {
+        return chambreService.addChambre(chambre);
+    }
+
+    @Override
+    public Chambre updateChambre(Chambre chambre) {
+        return chambreService.updateChambre(chambre);
+    }
+
+    @Override
+    public Chambre getChambreById(Long id) {
+        return chambreService.getChambreById(id);
+    }
+
+    @Override
+    public void deleteChambre(Long id) {
+       chambreService.deleteChambre(id);
+    }
+
+    @Override
+    public List<Chambre> getAllChambres() {
+        return chambreService.getAllChambres();
+    }
+
 }
--- back-end/src/main/java/com/example/backend/service/Interface/ChambreService.java
+++ back-end/src/main/java/com/example/backend/service/Interface/ChambreService.java
@@ -1,4 +1,13 @@
 package com.example.backend.service.Interface;
 
+import com.example.backend.entity.Chambre;
+
+import java.util.List;
+
 public interface ChambreService {
+    public Chambre addChambre(Chambre chambre);
+    public Chambre updateChambre(Chambre chambre);
+    public Chambre getChambreById(Long id);
+    public void deleteChambre(Long id);
+    public List<Chambre> getAllChambres();
 }
