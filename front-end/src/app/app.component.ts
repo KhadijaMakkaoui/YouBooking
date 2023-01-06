29222055aa8297531b9b669fa14574211b2531d4
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-01-06 14:58:41 +0100

    Adding Navigation with Event Binding and ngIf to display hotels or reservation pages

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'YouBooking';
+loadedFeature = 'hotel';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
