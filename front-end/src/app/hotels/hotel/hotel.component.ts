dbdeb20e31ab15e93dcbebcac7852a02e845abe0
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-01-06 16:06:12 +0100

    display hotel list

--- front-end/src/app/hotels/hotel/hotel.component.ts
+++ front-end/src/app/hotels/hotel/hotel.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Hotels} from "../hotels.model";
 
 @Component({
   selector: 'app-hotel',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./hotel.component.css']
 })
 export class HotelComponent {
-
+@Input() h: Hotels = new Hotels('Hotel 1', 'This is a test hotel', 'https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp');
 }
