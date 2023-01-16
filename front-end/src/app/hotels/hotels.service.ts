001b7dbfff72d5bdff734dac2b4a8aeff2df7590
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-01-16 22:29:10 +0100

    Update hotelsService to get data from backenf by calling spring boot APIs

--- front-end/src/app/hotels/hotels.service.ts
+++ front-end/src/app/hotels/hotels.service.ts
@@ -1,23 +1,47 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Hotels} from "./hotels.model";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelsService {
-  hotels:Hotels[] = [
+  private baseURL = 'http://localhost:8080/api/v1/hotels';
+  constructor(private httpClient:HttpClient) { }
+  getHotelsList():Observable<Hotels[]>{
+    return this.httpClient.get<Hotels[]>(`${this.baseURL}`);
+  }
+  createHotel(hotel:Hotels):Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`,hotel);
+  }
+  getHotelById(id:number):Observable<Hotels>{
+    return this.httpClient.get<Hotels>(`${this.baseURL}/${id}`);
+  }
+  updateHotel(id:number,hotel:Hotels):Observable<Object>{
+    return this.httpClient.put(`${this.baseURL}/${id}`,hotel);
+  }
+  deleteHotel(id:number):Observable<Object>{
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
+  }
+}
+/*  hotels:Hotels[] = [
     new Hotels('Hotel 1', 'This is a test hotel', 'https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp'),
     new Hotels('Hotel 2', 'This is a test hotel', 'https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp'),
     new Hotels('Hotel 3', 'This is a test hotel', 'https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp'),
     new Hotels('Hotel 4', 'This is a test hotel', 'https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp')
-  ];
+  ];*/
+/*
   hotelSelected=new EventEmitter<Hotels>();
+*/
+/*
   constructor() { }
-  getHotels(){
+*/
+  /*getHotels(){
     //get a copy of the array
     return this.hotels.slice();
   }
   getHotel(index:number){
     return this.hotels[index];
-  }
-}
+  }*/
+
